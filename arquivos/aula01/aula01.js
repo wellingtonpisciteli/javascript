4cb2abb8a6abc3835ad8579594dd189625c734a7
Borges <115740592+wellingtonpisciteli@users.noreply.github.com>
2023-02-06 17:18:23 -0300

    aula12

--- arquivos/aula01/aula01.js
+++ arquivos/aula01/aula01.js
@@ -1,1 +1,12 @@
-console.log("Hello, World")
+"use strict"
+
+const objs = document.getElementsByTagName("div")
+const objs1 = [...document.getElementsByTagName("div")]
+
+objs1.forEach(element => {
+    console.log(element)
+    element.style.fontSize="40px"
+})
+
+console.log(objs)
+console.log(objs1)
