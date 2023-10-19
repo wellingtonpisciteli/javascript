5ff5cd6bb26b530c2f01f2d52e0277ac0fe72c4c
Borges <115740592+wellingtonpisciteli@users.noreply.github.com>
2023-10-18 21:15:24 -0300

    estudos 18/10

--- aulas_js/aula63.js
+++ aulas_js/aula63.js
@@ -1,3 +1,55 @@
+const f_tipoMilitar = document.querySelector("#F_tipoMilitar")
+const f_tipoNormal = document.querySelector("#F_tipoNormal")
+const f_blindagem = document.querySelector("#f_blindagem")
+const f_municao = document.querySelector("#f_municao")
+const f_nome = document.querySelector("#f_nome")
+const f_portas = document.querySelector("#f_portas")
+const carros = document.querySelector("#carros")
+const btn_addCarro = document.querySelector("#btn_addCarro")
+
+let a_carros = []
+
+f_tipoMilitar.addEventListener("click", (evt)=>{
+    f_nome.value = ""
+    f_portas.value = 0
+    f_blindagem.value = 0
+    f_municao.value = 0
+    f_blindagem.removeAttribute("disabled")
+    f_municao.removeAttribute("disabled")
+})
+
+f_tipoNormal.addEventListener("click", (evt)=>{
+    f_nome.value = ""
+    f_portas.value = 0
+    f_blindagem.value = 0
+    f_municao.value = 0
+    f_blindagem.setAttribute("disabled", "disabled")
+    f_municao.setAttribute("disabled", "disabled")
+})
+
+const GerenciarExibicoesCarros = ()=>{
+    carros.innerHTML = ""
+    a_carros.map((c)=>{
+        const div = document.createElement("div")
+        div.setAttribute("class", "carro")
+        div.innerHTML = `Nome: ${c.nome}<br/>portas: ${c.portas}<br/>
+        blindagem: ${c.blindagem}<br/>Munição: ${c.municao}`
+        carros.appendChild(div)
+    })
+    
+}
+
+btn_addCarro.addEventListener("click", (evt)=>{
+    if(f_tipoNormal.checked){
+        const c = new carro(f_nome.value, f_portas.value)
+        a_carros.push(c)
+    }else{
+        const c = new militar(f_nome.value, f_portas.value, f_blindagem.value, f_municao.value)
+        a_carros.push(c)  
+    }
+    GerenciarExibicoesCarros()
+})
+
 class carro{ // classe pai
     constructor(nome, portas){
         this.nome = nome
@@ -40,8 +92,6 @@ c2.setCor("verde")
 c2.atirar()
 c2.atirar()
 c2.atirar()
-
-
 
 console.log(`nome: ${c1.nome}\nportas: ${c1.portas}\nligado: ${(c1.ligado)?"Sim":"Não"}\nvelocidade: ${c1.vel}\ncor: ${c1.cor}`)
 
