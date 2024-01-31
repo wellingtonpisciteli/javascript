86a51b1f3bf4228c8d538a48048816832a4f6016
Borges <115740592+wellingtonpisciteli@users.noreply.github.com>
2024-01-30 21:37:01 -0300

    estudos 30/01

---
+++ aulas_js/aula87.js
@@ -0,0 +1,13 @@
+const url=document.getElementById("url")
+const btn_url=document.getElementById("btn_url")
+
+btn_url.addEventListener("click", (evt)=>{
+    // window.location="aula83.html" //serve para redirecionar para utra url
+    //window.location.replace("aula83.html") //serve para redirecionar e excluir a página antiga
+    //window.location.reload() //recarrega a pagina rapidamente
+    //window.history.back() //serve para voltar para pagina antiga do historico
+    //window.history.forward() //serve para ir para a pagina seguinte do historico
+    //window.history.go(1) //indica a proxima pagina que quero navegar
+    // console.log(url.value)
+    // window.location=url.value //para navegar para outra pagina
+})
