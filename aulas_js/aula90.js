0216b18922933435173b8184eedb699391b41d96
Borges <115740592+wellingtonpisciteli@users.noreply.github.com>
2024-02-19 19:56:44 -0300

    estudos 19/02

--- aulas_js/aula90.js
+++ aulas_js/aula90.js
@@ -1,21 +1,51 @@
 const objetos=document.getElementById("objetos")
 
-const computadores=[
-    {
-        cpu:"i9",
-        ram:"64gb",
-        hd:"2tb"
-    },
-    {
-        cpu:"i8",
-        ram:"60gb",
-        hd:"1tb"
-    },
-    {
-        cpu:"i7",
-        ram:"32gb",
-        hd:"500mb"
+const computador={
+    cpu:"i9",
+    ram:"64gb",
+    hd:"2tb",
+    info:function(){
+        console.log(`CPU: ${this.cpu}`)
+        console.log(`RAM: ${this.ram}`)
+        console.log(`HD: ${this.hd}`)
     }
+}
+
+computador.Monitor="32pol" //uma forma de adicionar uma propriedade
+delete(computador.hd) //para deletar um atributo
+//console.log(computador.cpu) //para chamar apenas uma propriedade
+
+const c1=Object.assign(computador) //serve para copiar os dados de outro elemento
+c1.info()
+const c2=Object.create(computador) // aqui criamos um novo objeto
+c2.info()
+
+const o1={obj1:"1"}
+const o2={obj2:"2"}
+const o3={obj3:"3"}
+const o4=Object.assign(o1, o2, o3)
+console.log(o4) //assign tbm serve para mesclar objetos
+
+const computadores=[
+{
+    cpu:"i8",
+    ram:"60gb",
+    hd:"1tb"
+},
+{
+    cpu:"i7",
+    ram:"32gb",
+    hd:"500mb"
+}
 ]
 
-objetos.innerHTML=JSON.stringify(computadores)
+computadores.map((c)=>{
+    console.log(c)
+    const div=document.createElement("div")
+    div.innerHTML=c.cpu+"<br/>"+c.ram+"<br/>"+c.hd
+    div.setAttribute("class", "computador")
+    objetos.appendChild(div)
+})
+
+// computador.info()
+// objetos.innerHTML=JSON.stringify(computadores)
