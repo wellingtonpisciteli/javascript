ac5d636c569708b1578a23d5c0e472ae2cbd482c
Borges <115740592+wellingtonpisciteli@users.noreply.github.com>
2024-02-21 22:41:28 -0300

    Estudos 21/02

---
+++ aulas_js/aula91.js
@@ -0,0 +1,3 @@
+import Todos_Cursos,{ cursos,carros } from "./cursos.js"; //para exportar o padrao
+
+console.log(cursos,carros,Todos_Cursos(2))
---
+++ aulas_js/cursos.js
@@ -0,0 +1,10 @@
+const cursos=["JavaScript","PHP","Laravel","CSS","HTML","Bootstrap","MySQL","Java"]
+const carros=["Polo","Siena","Gol","Palio","Civic","Corola","Mustang","Lambo"]
+
+const Todos_Cursos=(pos)=>{
+    return cursos[pos]
+}
+
+export{cursos,carros}
+export default Todos_Cursos //metodo para deixar um objtivo como padrao no arquivo
+
