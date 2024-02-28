730eb8df518d2dd8ee30b583ad1a5b5b2214a9dd
Borges <115740592+wellingtonpisciteli@users.noreply.github.com>
2024-02-27 21:23:41 -0300

    estudos 27/02

--- aulas_js/aula91.js
+++ aulas_js/aula91.js
@@ -1,13 +1,30 @@
 import { contato } from "./contatos.js" 
+
 const lista_contatos=document.getElementById("lista_contatos")
 const btn_gravar=document.getElementById("btn_gravar")
+const destino_apagar=document.getElementById("destino_apagar")
+const nome=document.getElementById("f_nome")
+const tel=document.getElementById("f_telefone")
+const email=document.getElementById("f_email")
+
 
 btn_gravar.addEventListener("click", (evt)=>{
-    const conts={
-        nome:document.getElementById("f_nome").value,
-        telefone:document.getElementById("f_telefone").value,
-        email:document.getElementById("f_email").value
+    if(nome.value==""){
+        alert("Preencha os campos a seguir")
+    }else if(tel.value==""){
+        alert("Preencha os campos a seguir")
     }
-    contato.add_contato(conts, lista_contatos)
-    console.log(contato.todos_contatos())
+    else if(email.value==""){
+        alert("Preencha os campos a seguir")
+    }else{
+        const conts={
+            nome:document.getElementById("f_nome").value,
+            telefone:document.getElementById("f_telefone").value,
+            email:document.getElementById("f_email").value
+        }
+        contato.add_contato(conts, lista_contatos, destino_apagar)
+    }
+    nome.value=""
+    tel.value=""
+    email.value=""
 })
--- aulas_js/contatos.js
+++ aulas_js/contatos.js
@@ -4,29 +4,59 @@ let contato={
   todos_contatos:function(){
     return contatos
   },
+  
   i_contato:function(i){
     return contatos[i]
   },
-  add_contato:function(novo_contato,destino_dom){
+
+  add_contato:function(novo_contato,destino_dom, destino_apagar){
     const cont={
         nome:novo_contato.nome,
         telefone:novo_contato.telefone,
         email:novo_contato.email
     }
+
     contatos.push(cont)
 
-    const div=document.createElement("div")
-    div.setAttribute("class", "contato")
-    const p_nome=document.createElement("p")
-    p_nome.innerHTML=novo_contato.nome
-    const p_telefone=document.createElement("p")
-    p_telefone.innerHTML=novo_contato.telefone
-    const p_email=document.createElement("p")
-    p_email.innerHTML=novo_contato.email 
-    div.appendChild(p_nome)   
-    div.appendChild(p_telefone)   
-    div.appendChild(p_email)   
-    destino_dom.appendChild(div)
+    destino_dom.innerHTML=""
+    destino_apagar.innerHTML = ""
+
+    contatos.map((c, indice)=>{
+      console.log(indice)
+      
+      if(c.nome!=undefined){
+        let div=document.createElement("div")
+        div.setAttribute("class", "contato")
+        const p_nome=document.createElement("p")
+        p_nome.innerHTML=c.nome
+        const p_telefone=document.createElement("p")
+        p_telefone.innerHTML=c.telefone
+        const p_email=document.createElement("p")
+        p_email.innerHTML=c.email 
+        div.appendChild(p_nome)   
+        div.appendChild(p_telefone)   
+        div.appendChild(p_email)
+        destino_dom.appendChild(div) 
+
+        let btn_apag=document.createElement("button")
+        btn_apag.setAttribute("id","btn_apaga")
+        btn_apag.setAttribute("class","btn_apaga")
+        btn_apag.innerHTML="Apagar"
+        destino_apagar.appendChild(btn_apag) 
+
+        let percorre_contato=document.querySelectorAll(".contato")
+
+        percorre_contato.forEach((s)=>{
+          s.addEventListener("click",(evt, p)=>{
+            s.classList.add("selecionada")
+            btn_apag.addEventListener("click",()=>{
+              s.remove()
+              contatos.splice(p, 1)
+            })
+          })
+        })
+      }
+    })
   }
 }
 
