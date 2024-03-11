45d09a9b7b856098158eec6019bb6e15996df165
Borges <115740592+wellingtonpisciteli@users.noreply.github.com>
2024-03-11 20:07:21 -0300

    estudos 11/03

--- aulas_js/bancoDOM.js
+++ aulas_js/bancoDOM.js
@@ -3,10 +3,21 @@ import { Banco } from "./banco.js";
 const btnAbrir=document.getElementById("btnAbrir")
 const telaDireita=document.getElementById("telaDireita")
 const cadastrar=document.getElementById("cadastrar")
+const telaBtnCriar=document.getElementById("telaBtnCriar")
+const telaImagem=document.getElementById("telaImagem")
+const imgVolta=document.getElementById("imgVolta")
 
 
 btnAbrir.addEventListener("click", ()=>{
     telaDireita.style.display="none"
     cadastrar.style.display="flex"
+    telaBtnCriar.style.display="none"
+    telaImagem.style.display="flex"
+})
 
+imgVolta.addEventListener("click", ()=>{
+    telaBtnCriar.style.display="flex"
+    telaImagem.style.display="none"
+    cadastrar.style.display="none"
+    telaDireita.style.display="flex"
 })
