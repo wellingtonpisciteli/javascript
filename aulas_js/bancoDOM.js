002e406e10bcc621547a9248522f61a33e03a04d
Borges <115740592+wellingtonpisciteli@users.noreply.github.com>
2024-04-10 18:53:18 -0300

    comit 10/04

--- aulas_js/bancoDOM.js
+++ aulas_js/bancoDOM.js
@@ -1,11 +1,13 @@
-import { Banco } from "./banco.js";
-
 const btnAbrir=document.getElementById("btnAbrir")
 const telaDireita=document.getElementById("telaDireita")
 const cadastrar=document.getElementById("cadastrar")
 const telaBtnCriar=document.getElementById("telaBtnCriar")
 const telaImagem=document.getElementById("telaImagem")
 const imgVolta=document.getElementById("imgVolta")
+const cadastrar02=document.getElementById("cadastrar02")
+const proximo=document.getElementById("proximo")
+const anterior=document.getElementById("anterior")
+const nome=document.getElementById("nome")
 
 
 btnAbrir.addEventListener("click", ()=>{
@@ -13,11 +15,28 @@ btnAbrir.addEventListener("click", ()=>{
     cadastrar.style.display="flex"
     telaBtnCriar.style.display="none"
     telaImagem.style.display="flex"
+    
+    proximo.addEventListener("click",()=>{
+        if(nome.value==""){
+            alert("PREENCHA OS CAMPOS!")
+        }else{
+            cadastrar.style.display="none"
+            cadastrar02.style.display="flex"
+        }
+        
+        anterior.addEventListener("click",()=>{
+            cadastrar02.style.display="none"
+            cadastrar.style.display="flex"
+        })
+    })
+    imgVolta.addEventListener("click", ()=>{
+        telaBtnCriar.style.display="flex"
+        telaImagem.style.display="none"
+        cadastrar.style.display="none"
+        telaDireita.style.display="flex"
+        cadastrar02.style.display="none"
+    })
 })
 
-imgVolta.addEventListener("click", ()=>{
-    telaBtnCriar.style.display="flex"
-    telaImagem.style.display="none"
-    cadastrar.style.display="none"
-    telaDireita.style.display="flex"
-})
+
+
