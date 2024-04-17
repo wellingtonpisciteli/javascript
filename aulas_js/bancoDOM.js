953804d6ce3b681a7b51ce74708fb7d629fed946
Borges <115740592+wellingtonpisciteli@users.noreply.github.com>
2024-04-16 22:03:01 -0300

    atualizaçoes

--- aulas_js/bancoDOM.js
+++ aulas_js/bancoDOM.js
@@ -9,14 +9,18 @@ const proximo=document.getElementById("proximo")
 const anterior=document.getElementById("anterior")
 const nome=document.getElementById("nome")
 const nomeUser=document.getElementById("nomeUser")
+const senha=document.getElementById("senha")
 const telefone=document.getElementById("telefone")
+const email=document.getElementById("email")
 const criar=document.getElementById("criar")
 const carregando=document.getElementById("carregando")
 const mensagem=document.getElementById("mensagem")
 const dentroBanco=document.getElementById("dentroBanco")
 const cartao=document.getElementById("cartao")
+const perfil=document.getElementById("perfil")
+const sair=document.getElementById("sair")
 
-
+let controle=0
 
 btnAbrir.addEventListener("click", ()=>{
     telaDireita.style.display="none"
@@ -40,15 +44,15 @@ btnAbrir.addEventListener("click", ()=>{
     criar.addEventListener("click", ()=>{
         cadastrar02.style.display="none"
         carregando.style.display="flex"
+        mensagem.textContent="Carregando..."
         mensagem.style.display="flex"
-        console.log(nome.value)
         setTimeout(function(){
             mensagem.style.display="none"
             carregando.style.display="none"
             dentroBanco.style.display="flex"
 
             let header=document.createElement("h1")
-            header.textContent="Bem vindo "+nomeUser.value
+            header.textContent="Bem vindo ao YaYaBank "
             header.classList.add("headerDinamic")
             dentroBanco.appendChild(header)
 
@@ -61,8 +65,46 @@ btnAbrir.addEventListener("click", ()=>{
             nomeVisor.classList.add("nomeVisor")
             nomeVisor.textContent=nome.value
             cartao.appendChild(nomeVisor)
-        },3000)
-     
+
+            let perfilDinamic=document.createElement("div")
+            perfilDinamic.style.display="none"
+            perfil.addEventListener("click",()=>{
+                cartao.style.display="none"
+                header.style.display="none"
+                perfilDinamic.style.display="flex"
+                perfilDinamic.classList.add("perfilDinamic")
+                dentroBanco.appendChild(perfilDinamic)
+
+                let perfilInfo=document.createElement("div")
+                perfilInfo.classList.add("perfilInfo")
+                perfilDinamic.appendChild(perfilInfo)
+                let nomeInfo=document.createElement("input")
+                let labelNome=document.createElement("label")
+                labelNome.textContent="Nome"
+                nomeInfo.value=nome.value
+                nomeInfo.classList.add("inputInfo")
+                let teleInfo=document.createElement("input")
+                teleInfo.value=telefone.value
+                teleInfo.classList.add("inputInfo")
+                let emailInfo=document.createElement("input")
+                emailInfo.value=email.value
+                emailInfo.classList.add("inputInfo")
+                let senhaInfo=document.createElement("input")
+                senhaInfo.value="********"
+                senhaInfo.classList.add("inputInfo")
+                let alterarInfo=document.createElement("button")
+                alterarInfo.textContent="Alterar"
+                perfilInfo.appendChild(labelNome)
+                perfilInfo.appendChild(nomeInfo)
+                perfilInfo.appendChild(teleInfo)
+                perfilInfo.appendChild(emailInfo)
+                perfilInfo.appendChild(senhaInfo)
+                perfilInfo.appendChild(alterarInfo)
+            })
+    
+            sair.style.display="flex"
+            sair.classList.add("sair")
+        },3000)   
     } )
 
     imgVolta.addEventListener("click", ()=>{
@@ -72,6 +114,26 @@ btnAbrir.addEventListener("click", ()=>{
         telaDireita.style.display="flex"
         cadastrar02.style.display="none"
     })
+
+    sair.addEventListener("click", ()=>{
+        sair.style.display="none"
+        dentroBanco.style.display="none"
+        telaImagem.style.display="none"
+        carregando.style.display="flex"
+        mensagem.textContent="Saindo..."
+        mensagem.style.display="flex"
+        setTimeout(function(){
+            carregando.style.display="none"
+            mensagem.style.display="none"
+            telaBtnCriar.style.display="flex"
+            telaDireita.style.display="flex"
+            nome.value=""
+            telefone.value=""
+            email.value=""
+            nomeUser.value=""
+            senha.value=""
+        },3000)   
+})
 })
 
 
